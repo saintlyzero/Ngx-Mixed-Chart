6245e3678fba40ec2693526643a820c61d643b4a
saintlyzero <shubham.dalvi97@gmail.com>
2019-11-07 12:28:51 +0530

    Barchart with Line chart working build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import {ComboChartComponent, ComboSeriesVerticalComponent} from './combo-chart'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComboChartComponent, 
+    ComboSeriesVerticalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
